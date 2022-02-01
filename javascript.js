3b0f2f7f62ebc07a76c23290b5bf6ced6cb701d3
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-02-01 14:19:39 -0800

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,69 @@
+let now = new Date();
+
+let h6 = document.querySelector("h6");
+
+let hours = now.getHours();
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+h6.innerHTML = `${day} ${hours}:${minutes}`;
+
+function showWeather(response) {
+  document.querySelector("#cityName").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(cityName) {
+  let apiKey = "cf0f1f173fb62dd2bd98180f65a77eaf";
+  let units = "metric";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=";
+  let apiUrl = `${apiEndpoint}${cityName}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function goSearch(event) {
+  event.preventDefault();
+  let cityName = document.querySelector("#cityinput").value;
+  searchCity(cityName);
+}
+
+function retrievePosition(position) {
+  let apiKey = "cf0f1f173fb62dd2bd98180f65a77eaf";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+function gpsLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", goSearch);
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", gpsLocation);
+
+searchCity("Paris");
