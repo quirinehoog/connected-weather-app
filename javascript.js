f322d75770bc336c629b7331ca9cef931fb8ee19
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-02-02 17:57:16 -0800

    hours of the day updated with 0

--- javascript.js
+++ javascript.js
@@ -3,7 +3,8 @@ let now = new Date();
 let h6 = document.querySelector("h6");
 
 let hours = now.getHours();
-
+if (hours < 10) {
+  hours = `0${hours}`;
 let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
