efc8559b8e2ce27d2bd29bc8559fb2632885a763
quirinehoog <97471566+quirinehoog@users.noreply.github.com>
2022-02-03 15:58:06 -0800

    updating Javascript changing accurate date. not working jet

--- javascript.js
+++ javascript.js
@@ -5,45 +5,45 @@ let h6 = document.querySelector("h6");
 let hours = now.getHours();
 if (hours < 10) {
   hours = `0${hours}`;
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day}${hours}:$minutes}`;
 }
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-h6.innerHTML = `${day} ${hours}:${minutes}`;
 
 function showWeather(response) {
   let cityElement = document.querySelector("#cityName");
-  let temperatureElement= document.querySelector("#temperature"); 
-  let descriptionElement=document.querySelector("#description");
-  let humidityElement=document.querySelector("#humidity");
-  let windElement=document.querySelector("#wind");
-  let iconElement=document.querySelector("#icon");
- 
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+  let nowElement = document.querySelector("#now");
+
   cityElement.innerHTML = response.data.name;
-  temperatureElement.innerHTML = Math.round(
-    response.data.main.temp
-  );
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML =  Math.round(
-    response.data.wind.speed
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  nowElement.innerHTML = formatNow(response.data.dt * 1000);
 }
-
 
 function searchCity(cityName) {
   let apiKey = "cf0f1f173fb62dd2bd98180f65a77eaf";
@@ -102,3 +102,5 @@ tempFah.addEventListener("click", displayFahrenheitTemperature);
 
 let tempCel = document.querySelector("#tempcel");
 tempCel.addEventListener("click", displayCelsiusTemperature);
+
+searchCity("New York");
